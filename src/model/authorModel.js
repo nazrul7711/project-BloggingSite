19e660129d275cfff26b0d0a0cd9758ee815dcad
Nazrul7711 <nazrul7711@gmail.com>
2022-07-28 14:27:56 +0530

    schema added

--- src/model/authorModel.js
+++ src/model/authorModel.js
@@ -1,1 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+
+
+const authorSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: 'First name is required', //(sonu)
+        trim: true
+    },
+    lname: {
+        type: String,
+        required: 'Last name is required',
+        trim: true
+    },
+    title: {
+        type: String,
+        required: 'Title is required',
+        enum: ["Mr","Mrs","Miss","Mast"]
+    },
+    email: {
+        type: String,
+        lowercase:true,
+        required:'Email address is required',
+        unique: true,
+        trim: true,
+        validate:{
+            validator: function(email){
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            },message:'Please fill a valid email address', isAsync:false
+        }
+    },
+    password: {
+        type: String,
+        required: 'Password is required',
+        trim: true
+    }
+},{timestamps: true})
+
+module.exports = mongoose.model('Author',authorSchema)
